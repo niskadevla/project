ad1b48d820a14c2b99aab493957cbf95ce7a617d
Denis Beleshev <niskadevla@gmail.com>
2019-09-19 23:05:54 +0300

    сформированы все файлы

---
+++ js/sliders.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $('.header .slider-inner').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: false,
+    arrows: false,
+    dots: true,
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear'
+  });
+
+  $('.offers .slider-inner').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    lazyLoad: 'ondemand',
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
+});
